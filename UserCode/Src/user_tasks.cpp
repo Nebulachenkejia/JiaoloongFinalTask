0320b6787004648c31899542fa4a6cadb8128741
Nebulachenkejia <jonesnebula1209@gmail.com>
2025-11-11 18:17:06 +0800

    feat:构建一个双线程消息队列

--- UserCode/Src/user_tasks.cpp
+++ UserCode/Src/user_tasks.cpp
@@ -1,27 +1,44 @@
 //
 // Created by Nebula on 2025/11/5.
 //
+#include "cmsis_os2.h"
 #include "user_tasks.h"
 
-uint32_t count = 0;
+uint32_t send = 0;
+uint32_t recv = 0;
 
-osThreadId_t testTaskHandler;
+osMessageQueueAttr_t test_queue_attributes = { .name = "test_queue"};
+osMessageQueueId_t test_queue_handle;
+
+osThreadId_t test1TaskHandle;
 constexpr osThreadAttr_t testTask_attributes = {
     .name = "testTask",
     .stack_size = 256 * 4,
     .priority = osPriorityAboveNormal
 };
 
-void test_tasks(void *){
+void test1_tasks(void *){
     while(true){
         const auto tick = osKernelGetTickCount();
-        ++count;
+        ++send;
+        osMessageQueuePut(test_queue_handle, &send, 0, 0);
         osDelayUntil(tick + 1);
     }
 }
 
+osThreadId_t test2TaskHandle;
+void test2_tasks(void *){
+    while(true){
+        osMessageQueueGet(test_queue_handle, &recv, nullptr, osWaitForever);
+    }
+}
+
+
 
 void user_tasks_init()
 {
-    testTaskHandler = osThreadNew(test_tasks, nullptr, &testTask_attributes);
+    test_queue_handle = osMessageQueueNew(10, sizeof(uint32_t), &test_queue_attributes);
+    test1TaskHandle = osThreadNew(test1_tasks, nullptr, &testTask_attributes);
+    test2TaskHandle = osThreadNew(test2_tasks, nullptr, &testTask_attributes);
+
 }
